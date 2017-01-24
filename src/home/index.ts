1f973463c6865ae5d02924cb2147783eb1a55dc8
Sn0wFox <ruben.pericas@laposte.net>
2017-01-24 19:47:50 +0100

    Restaured autoscroll on discover button

--- src/home/index.ts
+++ src/home/index.ts
@@ -1,9 +1,22 @@
-import { initNavBar } from '../common/common';
+import { initNavBar,
+  runAnimatedScroll } from '../common/common';
 
 // Cache for jquery selectors
 let $document = $(document);
+let $discover = $('#discover');
 
 // On document ready
 $document.ready(() => {
+  // Init navbar
   initNavBar('#siteNav', 1250);
+
+  // Init discover button
+  $discover.on('click', (event: any) => {
+      // Make sure this.hash has a value before overriding default behavior
+      if (event.target.hash && event.target.hash !== '') {
+        // Prevent default anchor click behavior
+        event.preventDefault();
+        runAnimatedScroll(event.target.hash, 1000);
+      }
+  });
 });
