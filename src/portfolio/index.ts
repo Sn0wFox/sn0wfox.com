ce37a6fbdd20da1381bc9ab9807f2da898c76e7b
Sn0wFox <ruben.pericas@laposte.net>
2017-01-24 22:02:51 +0100

    Added peudo lrc pictures preview

--- src/portfolio/index.ts
+++ src/portfolio/index.ts
@@ -6,4 +6,10 @@ let $document = $(document);
 // On document ready
 $document.ready(() => {
   initNavBar('#siteNav', 1250);
+
+  $("#snfx-lrc-pictures").find("a").on("click", (event: any) => {
+    console.log(event.currentTarget.attributes['href'].value);
+    $("#imagepreview").attr('src', event.currentTarget.attributes['href'].value);
+    $('#imagemodal').modal('show');
+  });
 });
