da91e98056b6d0bc4180d4e5aa33267ed67ff159
Sn0wFox <ruben.pericas@laposte.net>
2017-01-25 10:38:13 +0100

    Reworked portfolio intro

--- src/portfolio/index.ts
+++ src/portfolio/index.ts
@@ -1,12 +1,26 @@
-import { initNavBar } from '../common/common';
+import { initNavBar,
+  runAnimatedScroll } from '../common/common';
 
 // Cache for jquery selectors
 let $document = $(document);
+let $chkbtn = $('#snfx-btn-chekit');
 
 // On document ready
 $document.ready(() => {
+  // Init navbar
   initNavBar('#siteNav', 1250);
 
+  // Init check it out button
+  $chkbtn.on('click', (event: any) => {
+    // Make sure this.hash has a value before overriding default behavior
+    if (event.target.hash && event.target.hash !== '') {
+      // Prevent default anchor click behavior
+      event.preventDefault();
+      runAnimatedScroll(event.target.hash, 1000);
+    }
+  });
+
+  // Init modal LRC pictures
   $("#snfx-lrc-pictures").find("a").on("click", (event: any) => {
     console.log(event.currentTarget.attributes['href'].value);
     $("#imagepreview").attr('src', event.currentTarget.attributes['href'].value);
