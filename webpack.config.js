01eda2aea6ee068e947d99cee81aaf72858d215c
Sn0wFox <ruben.pericas@laposte.net>
2017-10-11 17:46:05 +0200

    Add gulp helpers section

--- webpack.config.js
+++ webpack.config.js
@@ -1,5 +1,4 @@
 const path    = require('path');
-const util    = require('util');
 const webpack = require('webpack');
 
 const UglifyJSPlugin        = require('uglifyjs-webpack-plugin');
@@ -113,42 +112,3 @@ module.exports = {
     ignored: /node_modules/
   }
 };
-
-
-// // Our Webpack Defaults
-// let defaultConfig = {
-//   devtool: 'source-map',
-//
-//   output: {
-//     filename: '[name].bundle.js',
-//     sourceMapFilename: '[name].map',
-//     chunkFilename: '[id].chunk.js'
-//   },
-//
-//   resolve: {
-//     extensions: [ '.ts', '.js' ],
-//     modules: [ path.resolve(__dirname, 'node_modules') ]
-//   },
-//
-//   devServer: {
-//     historyApiFallback: true,
-//     watchOptions: { aggregateTimeout: 300, poll: 1000 },
-//     headers: {
-//       "Access-Control-Allow-Origin": "*",
-//       "Access-Control-Allow-Methods": "GET, POST, PUT, DELETE, PATCH, OPTIONS",
-//       "Access-Control-Allow-Headers": "X-Requested-With, content-type, Authorization"
-//     }
-//   },
-//
-//   node: {
-//     global: true,
-//     crypto: 'empty',
-//     __dirname: true,
-//     __filename: true,
-//     process: true,
-//     Buffer: false,
-//     clearImmediate: false,
-//     setImmediate: false
-//   }
-// };
-
