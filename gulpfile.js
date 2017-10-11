fbfb2d70a84a2fb5571593a867776854c436f0d0
Sn0wFox <ruben.pericas@laposte.net>
2017-10-11 16:13:42 +0200

    Update build flow

--- gulpfile.js
+++ gulpfile.js
@@ -1,79 +1,160 @@
-"use strict";
+const gulp      = require('gulp');            // Local gulp lib
+const gsass     = require('gulp-sass');       // To compile sass & scss files
+const gpug      = require('gulp-pug');        // To compile pug files
+const gwebpack  = require('webpack-stream');  // To use webpack with gulp
+const gutil     = require('gulp-util');       // To log anything gulp style
 
-const gulp = require('gulp');                     // Local gulp lib
-const gsass = require('gulp-sass');               // To compile sass & scss files
-const gpug = require('gulp-pug');                 // To compile pug files
-const gwebpack = require('webpack-stream');       // To use webpack with gulp
+const del       = require('del');             // To erase some file during cleaning tasks
+const path      = require('path');            // To manage path expressions correctly
+const webpack   = require('webpack');         // Local webpack lib
+const notifier  = require('node-notifier');   // To show notifications
 
-const webpack = require('webpack');               // Local webpack lib
-const del = require('del');                       // To erase some file during cleaning tasks
+require('colors');
 
-const wpconf = require('./webpack.config.js');
+/****************************
+ * CONFIG
+ ****************************/
 
-/**
- * Compiles TypeScript files from src/
- * using the typings.
- */
-gulp.task('build:ts', () => {
-  return gulp
-    .src(['src/**/*.ts'])
-    .pipe(gwebpack(wpconf, webpack))
-    .pipe(gulp.dest('dist'));
-});
+const wpconf        = require('./webpack.config.js');         // Local webpack config
+const DEV           = process.env.SNFX_BUILD_MODE === 'dev';  // Dev mode or not
 
-/**
- * Compiles Pug files at the root of src/.
- */
-gulp.task('build:pug', () => {
-  return gulp
-    .src('src/**/*.pug')
-    .pipe(gpug())
-    .pipe(gulp.dest('dist'));
-});
+const SRC_ROOT      = 'src';
+const DIST_ROOT     = 'dist';
+const ASSETS_FOLDER = 'assets';
+const ASSETS_ROOT   = path.join(SRC_ROOT, ASSETS_FOLDER);
+
+
+/****************************
+ * PUBLIC TASKS
+ ****************************/
 
 /**
- * Compiles Sass (.sass and .scss) files at the root of src/.
+ * Builds all files needed an watch non-Ts/Js files.
+ * If the build mode is set to dev it also enables
+ * build notifications and Webpack watcher.
  */
-gulp.task('build:sass', () => {
-  return gulp
-    .src(['src/**/*.scss', 'src/**/*.sass'])
-    .pipe(gsass())
-    .pipe(gulp.dest('dist'));
-});
+gulp.task('build', (done) => {
+  gulp.parallel(buildPug, buildSass, buildAssets)((error) => {
+    if(DEV) {
+      let notifierOptions = {
+        title: 'Sn0wFox.com builder',
+        sound: false
+      };
+      notifierOptions.message = error ? '[ERROR] Build failed. See terminal.' : 'Build successful.';
+      notifierOptions.icon = error ? path.join(__dirname, ASSETS_ROOT, 'images/logo-black.png') : path.join(__dirname, ASSETS_ROOT, 'images/logo.png');
+      notifier.notify(notifierOptions);
+    }
 
-/**
- * Copies static files (e.g. pictures, .ico and things needed)
- * from src/assets into dist/.
- */
-gulp.task('build:assets', () => {
-  return gulp
-    .src('src/assets/**/*')
-    .pipe(gulp.dest('dist/assets'));
+    if(error) {
+      gutil.log('[ERROR]'.red, error);
+    }
+
+    gulp.parallel('watch', buildTs)(done);
+  });
 });
 
 /**
  * Cleans the dist folder by removing it.
  */
 gulp.task('clean', () => {
-  return del('dist/**/*');
+  return del(path.join(DIST_ROOT, '**/*'));
 });
 
 /**
- * Builds all files needed.
- */
-gulp.task('build', gulp.parallel(
-  'build:pug',
-  'build:sass',
-  'build:assets',
-  'build:ts'));
-
-/**
- * Watches all .sass .scss .pug .ts files and assets folder,
- * and recompiles/copies them when they change.
+ * Watches all .sass .scss .pug files and assets folder,
+ * and re-compiles/copies them when they change.
  */
 gulp.task('watch', () => {
-  gulp.watch(['src/**/*.scss', 'src/**/*.sass'], gulp.series('build:sass'));
-  gulp.watch(['src/**/*.pug'], gulp.series('build:pug'));
-  gulp.watch(['src/**/*.ts'], gulp.series('build:ts'));
-  gulp.watch(['src/assets/**/*'], gulp.series('build:assets'));
+  process.env.SNFX_WATCH = true;
+  gulp.watch([path.join(SRC_ROOT, '/**/*.scss'), path.join(SRC_ROOT, '/**/*.sass')], buildSass);
+  gulp.watch([path.join(SRC_ROOT, '/**/*.pug')], buildPug);
+  gulp.watch([path.join(ASSETS_ROOT, '/**/*')], buildAssets);
 });
+
+/****************************
+ * PRIVATE TASKS
+ ****************************/
+
+/**
+ * Compiles TypeScript files from src/
+ * using the typings.
+ */
+function buildTs() {
+  return gulp
+    .src([path.join(SRC_ROOT, '/**/*.ts')])
+    .pipe(gwebpack(wpconf, webpack))
+    .pipe(gulp.dest(DIST_ROOT));
+}
+Object.defineProperty(buildTs, 'name', {value: 'build:ts'});
+
+/**
+ * Compiles Pug files at the root of src/.
+ */
+function buildPug() {
+  let error = null;
+  return gulp
+    .src(path.join(SRC_ROOT, '/**/*.pug'))
+    .pipe(gpug())
+    .on('error', function(err) {
+      error = err;
+      gutil.log('[ERROR]'.red, error.message);
+      this.emit('end');
+    })
+    .on('end', () => notify('Pug', error))
+    .pipe(gulp.dest(DIST_ROOT));
+}
+Object.defineProperty(buildPug, 'name', {value: 'build:pug'});
+
+/**
+ * Compiles Sass (.sass and .scss) files at the root of src/.
+ */
+function buildSass() {
+  let error = null;
+  return gulp
+    .src([path.join(SRC_ROOT, '/**/*.scss'), path.join(SRC_ROOT, '/**/*.sass')])
+    .pipe(gsass())
+    .on('error', function(err) {
+      error = err;
+      gutil.log('[ERROR]'.red, error.message);
+      this.emit('end');
+    })
+    .on('end', () => notify('Sass', error))
+    .pipe(gulp.dest(DIST_ROOT));
+}
+Object.defineProperty(buildSass, 'name', {value: 'build:sass'});
+
+/**
+ * Copies static files (e.g. pictures, .ico and things needed)
+ * from src/assets into dist/.
+ */
+function buildAssets() {
+  let error = null;
+  return gulp
+    .src(path.join(ASSETS_ROOT, '/**/*'), {base: SRC_ROOT})
+    .on('error', function(err) {
+      error = err;
+      gutil.log('[ERROR]'.red, error.message);
+      this.emit('end');
+    })
+    .on('end', () => notify('Assets', error))
+    .pipe(gulp.dest(DIST_ROOT));
+}
+Object.defineProperty(buildAssets, 'name', {value: 'build:assets'});
+
+
+/**
+ * A generic function to send a notification to the user.
+ * @param module The module's name.
+ * @param error An optional error object.
+ */
+function notify(module, error) {
+  if(DEV && process.env.SNFX_WATCH) {
+    let notifierOptions = {
+      title: `Sn0wFox.com builder - ${module}`,
+      sound: false
+    };
+    notifierOptions.message = error ? `[ERROR] ${module} build failed. See terminal.` : `${module} build successful.`;
+    notifierOptions.icon = error ? path.join(__dirname, ASSETS_ROOT, 'images/logo-black.png') : path.join(__dirname, ASSETS_ROOT, 'images/logo.png');
+    notifier.notify(notifierOptions);
+  }
+}
